c12d549ae24b44b8b124462933520316a26c8cf8
maaaaaaaaad <integral5137@gmail.com>
2024-10-01 17:10:00 +0900

    e

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       
+        int[] ball = new int[45];
+        System.out.printf("Ball length: %d\n", ball.length);
+        for (int i = 0; i < ball.length; i++) {
+            ball[i] = i + 1;
+            System.out.printf("Ball: %d\n", ball[i]);
+        }
     }
 }
