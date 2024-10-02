0887bc593d883805c5a00225e68a10e69e595276
maaaaaaaaad <integral5137@gmail.com>
2024-10-02 20:13:53 +0900

    e

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int[] ball = new int[45];
         System.out.printf("Ball length: %d\n", ball.length);
-        for (int i = 0; i < ball.length; i++) {
+        for (int i = 0; i < ball.length; i++)
             ball[i] = i + 1;
-            System.out.printf("Ball: %d\n", ball[i]);
+        for (int i = 0; i < 5; i++) {
+            double k = Math.random() * 45;
+            int extract = ball[(int) k];
+            System.out.printf("Extracted: %d\n", extract);
         }
     }
 }
