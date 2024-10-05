6c50854148c3637a2cdfc8aa37a5429085ee6f6e
maaaaaaaaad <integral5137@gmail.com>
2024-10-05 21:22:49 +0900

    e

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Tv tv = new Tv();
         tv.channel = 7;
+        tv.channelDown();
+        System.out.println(tv.channel);
     }
 }
